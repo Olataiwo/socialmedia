84a29f3332f80ffde9c2eb9245977b2c9273829f
dimeji <dimeji.taiwo5@outlook.com>
2017-06-30 09:23:30 +0100

    modal works

--- www/my_laravel/resources/views/dashboard.blade.php
+++ www/my_laravel/resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
  @include('includes.messageblock')
 	<section class="row new-post" >
 
-		<div class = 'col-md-6 offset-md-3'>
+		<div class = 'col-md-6 .col-md-offset-3'>
 			<header>
 				<h3>Whats on your mind</h3>
 			</header>
@@ -18,7 +18,7 @@
 			</form>
 		</section>
 	<section class = 'row-post'>
-		<div 'col-md-6 offset-md-3'>
+		<div class ='col-md-6 .col-md-offset-3'>
 			<header><h3>What other people say...</h3></header>
 			@foreach($posts as $post)
 			<article class="post">
@@ -29,9 +29,9 @@
 			<div class= "interaction">
 				<a href="#">Like</a> |
 				<a href="#">Dislike</a> 
-				@if(Auth::user()==$post->user)
+				@if(Auth::user() == $post->user)
 				|  
-				<a href="#">Edit</a>   |
+				<a href="" class="edit">Edit</a>   |
 				<a href="{{route('post.delete',['post_id'=>$post->id])}}">Delete</a>  |
 				@endif
 			</div>
@@ -41,6 +41,31 @@
 			@endforeach
 			
 		</div>
+	<section>
+	<div class="modal fade" tabindex="-1" role="dialog" id='edit-modal'>
+  	<div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Edit Post</h4>
+      </div>
+      <div class="modal-body">
+        <form >
+        	<div = "form-group">
+        		<label for="post-body">Edit the Post</label>
+        		<textarea name="post-body" id="post-body" row="5"></textarea>
+        	</div>
+        	
+        	
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 
 
 	</section>
--- www/my_laravel/resources/views/layouts/master.blade.php
+++ www/my_laravel/resources/views/layouts/master.blade.php
@@ -6,9 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
 
-        <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
      
-        <link rel="stlesheet" href= "{{ URL::to('src/css/main.css')}}">
+        <link rel="stylesheet"  href= "{{asset('styles/app.css')}}" >
     </head>
     <body>
         @include('includes.header')
@@ -16,6 +16,8 @@
             @yield('content')
         </div>
         
-        <script src=""></script>
+        <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <script src="{{asset('js/modal.js')}}"></script>
     </body>
 </html>
